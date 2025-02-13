062776af921840f8aa38a0aa160135173dcb0d86
Iskhak <ishakishakov00@gmail.com>
2025-02-13 12:06:47 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,182 @@
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# for ss in a:
+#     if ss <  5:
+#         print(ss)
+
+
+# my_float = 1.64
+# my_integer = int(my_float) после точкадан кийин баарын алп салат
+# my_integer = round(my_float) арифметика мн иштейт
+
+# print (my_integer)
+
+# ee_int = 33 # квартира номер
+# nm_int = (ee_int - 1)  // 20 +1  # подиезд номер  
+# et_int = (ee_int - 1) % 20 // 4 +1 # этаж номер
+# print (nm_int)
+# print (et_int)
+
+
+
+ 
+
+# my_string =  input("My name is")
+# my_integer = int( input("My eayr my_list =[1,12,"edf"]
+# print(my_list)
+
+
+# big_int = 2 **1000
+# print( len( str( big_int)))
+
+# my_string = "Iskhak"
+# print(my_string.upper())  # баарнн чон кылат
+# print(my_string.lower())  # баарын кичиене кылат
+
+
+# my_string = "Hello, world!"
+# print(my_string.replace('world' , "Python"))  # replace алмаштырат
+
+
+
+# <-------------->
+# integer = input("Enter a number :")
+# if integer.isdigit():
+#     integer = int( integer)
+# print(type(integer))   
+
+# my_string = input("Enter a string :")
+
+# if my_string.isdigit():
+#       my_integer = int(my_string)
+#       print(my_integer)
+# else:
+#  print(f"{my_string} not a number")
+
+
+
+# fruties = ["Iskhak", "Mirlan", "keldibek"]
+# print("ikshak"in fruties )
+# print("Iskhak"in fruties)
+# print(fruties[1])
+
+
+# fruits = ["Banana", "Apfel","Kivi"]
+# fruits.append("Anar")
+# print(fruits)
+
+
+# Name = ["Iskhak", "Mirlan", "Keldibek"]
+# Name+ = Name.pop()#pop акыркы созду переменныйга айлантат
+# print(Name)
+# print(Name+)
+
+
+
+# Name = ["Iskhak", "Mirlan", "keldibek"]
+# Name+ = ["Batyrov","Burulay"]
+
+# Name.extend(Name+)# extend кошот бири бирине 
+# print(Name)
+
+# my_list = [12,312,11,23,123,5,123,2]
+# my_list.sort() # sort  сортировка кылат
+# print(my_list)
+
+
+# my_string ="My name is Iskhak"
+# my_list = my_string.split(" ") # split бул баарын болуп берет 
+# print(my_list)
+
+# my_strings = " ".join(my_list)  # split бул болгондорду кайра чогултуп берет
+# print(my_strings) 
+
+
+# my_list = [1,21,32,12,34,2,112]
+
+# print(max(my_list))
+# print(min(my_list))
+# print(sum(my_list))
+
+# my_list = [ 1,21,12,34,2,112 ]
+
+# print(my_list[:4]) 
+
+# my_list =[1,12,"edf"]
+# print(my_list)
+
+
+# my_list = True
+# my_int = 2 
+# my = my_list + my_int
+# print(my) 
+
+#<------------------>
+# my_sekund = 3681
+
+# minute = my_sekund // 60
+# sekund = my_sekund % 60
+# hour = minute // 60
+# print(minute )
+# print(sekund )
+# print(hour )
+
+# print(type(minute))
+# print(type(sekund))
+# print(type(hour))
+
+
+# my_sekund = input("Сан киргиз")
+# my_sekund = int(my_sekund)
+# hour = my_sekund // 3600
+# minute = (my_sekund % 3600) // 60 
+# sekund = ((my_sekund % 3600) % 60)
+# print(hour )
+# print(minute )
+# print(sekund )  
+
+
+
+# db_login = "foot"
+# db_pass = 1234
+# if (db_login=="foot"):
+#     print("professional")
+# elif (db_pass==1234):
+#     print("prof")
+# else:
+#         print("kk")
+
+
+
+# for i in range ( 0, 10 ):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+# while i < 10:
+#     if i % 2 == 0:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+
+# b=1
+# while b < 10:
+#     if b % 2 == 0:
+#         b += 1
+#         continue
+#     print(b)
+#     b += 1
+# print(b)
+
+
+
+#<--------================----------------->
+# dict
+#set 
+#tuple
+#list
+#<===================---------==============>
+
+
